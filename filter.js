6f2d49826706642249eb371e059ba4a32c987c94
ls4825783 <ls4825783@gmail.com>
2022-08-27 12:10:18 +0530

    first javascript with filterprogram

---
+++ filter.js
@@ -0,0 +1,17 @@
+const inputWords = ["spray", "limit", "elite", "exuberant", "destruction", "present"];
+let word = [];
+let j=0;
+for (i=0;i<inputWords.length;i++)
+{
+    let words = [];
+    
+   if (inputWords[i].length>6){
+       
+       words[j] = inputWords[i];
+       word[j]= words[j];
+       j++;
+        
+       }
+       
+}
+console.log(word);
